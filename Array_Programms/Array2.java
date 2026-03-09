d90c353f81a28367913c087f1ec4c6141b3f3a17
Sunilashokbhoi <sunilbhoi606@gmail.com>
2026-03-09 19:33:47 +0530

    ADDED: 2 Reverse Array

---
+++ Array_Programms/Array2.java
@@ -0,0 +1,11 @@
+package arrays.day3;
+
+public class Array2 {
+	public static void main(String[] args) {
+		int[] arr = {12,13,15,11,28,33,42,14};
+		for(int i=arr.length-1; i>=0 ;i--) {
+			System.out.println(arr[i]);
+		}
+		System.out.println("--------");
+	}
+}
